742fe6fc2de67997b1c4457aea2bc7ff2b86f3b5
cesare <cesaremannino@hotmail.it>
2021-06-28 08:12:52 +0000

    server

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ const exphbs = require("express-handlebars");
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
-require("dotenv").config();
+
 
 // Parsing middleware
 const app = express();
 //Listen on port 5000
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.use(express.urlencoded({extended: true})); //New
 app.use(express.json()); //To parse the incoming requests with JSON payloads
+app.use(express.urlencoded({extended: true})); //New
 
 
+require("dotenv").config();
 
 
 //make the home page as first page/landing page
 const routes = require('./server/routes/user');
-app.use('/', routes);
+app.get('/', (req, res) => {
+    res.render('home', {layout: 'main2'}) 
+})
 
 
 
@@ -38,3 +39,5 @@ app.set("view engine","hbs");
 
 
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
