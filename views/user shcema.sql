1a185c75743d398d26b9184722fc74646a7d0908
cesare <cesaremannino@hotmail.it>
2021-06-28 09:01:45 +0000

    user-shcema

--- views/user shcema.sql
+++ views/user shcema.sql
@@ -1,1 +1,14 @@
-CREATE TABLE `vesselmanagement_tut`.`user` ( `id` INT NOT NULL , `first_name` VARCHAR(45) NOT NULL , `last_name` VARCHAR(45) NOT NULL , `email` VARCHAR(45) NOT NULL , `phone` VARCHAR(45) NOT NULL , `comments` TEXT NOT NULL , `status` VARCHAR(10) NOT NULL DEFAULT 'active' , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS 
+`user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `phone` varchar(45) NOT NULL,
+  `coc` varchar(45) NOT NULL,
+  `expiration` varchar(19) NOT NULL,
+  `PSSR` varchar(11) NOT NULL,
+  `FFB` varchar(11) NOT NULL,
+  `ADV` varchar(11) NOT NULL,
+  `status` varchar(11) NOT NULL DEFAULT 'active'
+,PRIMARY KEY (`id`)) ENGINE=InnoDB;
