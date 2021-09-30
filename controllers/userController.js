0a6934ae184c74124f53ff84ee1c3115714fa87e
cesare <cesaremannino@hotmail.it>
2021-09-30 21:03:12 +0000

    connection

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,22 @@
 const mysql = require('mysql');
 
-let connection = mysql.createConnection({
+// let connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME
+//   });
+  
+  var db_config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
-  });
-  
+};
 
  
+var connection;
+
   function handleDisconnect() {
       connection = mysql.createConnection(db_config); // Recreate the connection, since
       // the old one cannot be reused.
