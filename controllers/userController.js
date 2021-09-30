60b4407bcd08b7d08261e703896a8e1a7e211997
cesare <cesaremannino@hotmail.it>
2021-09-30 22:06:04 +0000

    connection

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-var db_config = ({
+let connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
@@ -8,30 +8,7 @@ var db_config = ({
   });
   
 
- 
-  function handleDisconnect() {
-      connection = mysql.createConnection(db_config); // Recreate the connection, since
-      // the old one cannot be reused.
-  
-      connection.connect(function (err) {              // The server is either down
-          if (err) {                                     // or restarting (takes a while sometimes).
-              console.log('error when connecting to db:', err);
-              setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-          }                                     // to avoid a hot loop, and to allow our node script to
-      });                                     // process asynchronous requests in the meantime.
-      // If you're also serving http, display a 503 error.
-      connection.on('error', function (err) {
-          console.log('db error', err);
-          if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-              handleDisconnect();                         // lost due to either server restart, or a
-          } else {                                      // connnection idle timeout (the wait_timeout
-              throw err;                                  // server variable configures this)
-          }
-      });
-  }
-  
-  handleDisconnect();
-  
+
 
   
 connection.connect( (error) => {
@@ -41,10 +18,6 @@ connection.connect( (error) => {
         console.log('MySQL user Connected...')
     }
 });
-
-
-// DATABASE_URL: mysql://bbaaff48f634c6:dacbf7fa@us-cdbr-east-04.cleardb.com/heroku_c7ad469172e97f3?reconnect=true
-
 
 
 // View Users
