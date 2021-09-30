6f8d91489db6bfd066309ebbd1f9bc1d3ba9db45
cesare <cesaremannino@hotmail.it>
2021-09-30 09:36:41 +0000

    connection only user

--- app.js
+++ app.js
@@ -27,10 +27,10 @@ app.set("view engine","hbs");
 //link that tell to the server express.js to get the routeing from user.js
 // const routes = require('./server/routes/user');
 app.use("/",require('./routes/user'));
-app.use('/auth', require('./routes/auth'));
+// app.use('/auth', require('./routes/auth'));
 
 
 
 
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => console.log(`Listening on port ${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
