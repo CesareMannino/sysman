8bd663cba0c04c3002033492992c11656f9d7aff
cesare <cesaremannino@hotmail.it>
2021-09-30 10:03:53 +0000

    connection

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,38 +22,6 @@ const mysql = require('mysql');
 
 // DATABASE_URL: mysql://bbaaff48f634c6:dacbf7fa@us-cdbr-east-04.cleardb.com/heroku_c7ad469172e97f3?reconnect=true
 
-var db_config = {
-    host: "us-cdbr-east-04.cleardb.com",
-    user: "bbaaff48f634c6",
-    password: "dacbf7fa",
-    database: "heroku_c7ad469172e97f3"
-};
-
-var connection;
-
-function handleDisconnect() {
-    connection = mysql.createConnection(db_config); // Recreate the connection, since
-    // the old one cannot be reused.
-
-    connection.connect(function (err) {              // The server is either down
-        if (err) {                                     // or restarting (takes a while sometimes).
-            console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-        }                                     // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meantime.
-    // If you're also serving http, display a 503 error.
-    connection.on('error', function (err) {
-        console.log('db error', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-            handleDisconnect();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
-            throw err;                                  // server variable configures this)
-        }
-    });
-}
-
-handleDisconnect();
-
 
 
 // View Users
