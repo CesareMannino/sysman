5f30918d0d87533cf09aeaf26616a81d32a82422
cesare <cesaremannino@hotmail.it>
2021-10-06 09:52:48 +0200

    user.js changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,173 +3,173 @@ const mysql = require('mysql');
 const bcrypt = require('bcryptjs');
 const { promisify } = require('util');
 
-// var db_config = {
-//     host: "us-cdbr-east-04.cleardb.com",
-//     user: "bbaaff48f634c6",
-//     password: "dacbf7fa",
-//     database: "heroku_c7ad469172e97f3"
-// };
+var db_config = {
+    host: "us-cdbr-east-04.cleardb.com",
+    user: "bbaaff48f634c6",
+    password: "dacbf7fa",
+    database: "heroku_c7ad469172e97f3"
+};
 
-// var connection;
+var connection;
 
-// function handleDisconnect() {
-//     connection = mysql.createConnection(db_config); // Recreate the connection, since
-//     // the old one cannot be reused.
+function handleDisconnect() {
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+    // the old one cannot be reused.
 
-//     connection.connect(function (err) {              // The server is either down
-//         if (err) {                                     // or restarting (takes a while sometimes).
-//             console.log('error when connecting to db:', err);
-//             setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-//         }                                     // to avoid a hot loop, and to allow our node script to
-//     });                                     // process asynchronous requests in the meantime.
-//     // If you're also serving http, display a 503 error.
-//     connection.on('error', function (err) {
-//         console.log('db error', err);
-//         if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-//             handleDisconnect();                         // lost due to either server restart, or a
-//         } else {                                      // connnection idle timeout (the wait_timeout
-//             throw err;                                  // server variable configures this)
-//         }
-//     });
-// }
+    connection.connect(function (err) {              // The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+    // If you're also serving http, display a 503 error.
+    connection.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
 
-// handleDisconnect();
-
-
-// // code in case the user leave the login space empty
-
-// exports.login = async (req, res) => {
-//     try {
-//         const { email, password } = req.body;
-
-//         if (!email || !password) {
-//             return res.status(400).render('login', {
-//                 message: 'Please provide an email and password'
-//             })
-//         }
-
-//         connection.query('SELECT * FROM login WHERE email=?', [email], async (error, results) => {
-//             console.log(results);
-//             if (!results || !(await bcrypt.compare(password, results[0].password))) {
-//                 res.status(401).render('login', {
-//                     message: 'Email or Password is incorrect'
-//                 })
-//             } else {
-//                 const id = results[0].id;
-//                 const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-//                     expiresIn: process.env.JWT_EXPIRES_IN
-//                 });
-
-//                 console.log('the token is:' + token);
-//                 const cookieOptions = {
-//                     expires: new Date(
-//                         Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
-//                     ),
-//                     httpOnly: true
-//                 }
-//                 res.cookie('jwt', token, cookieOptions);
-//                 res.status(200).redirect('/ui');
-//             }
-
-//         });
-
-//     } catch (error) {
-//         console.log(error)
-//     }
-
-// }
+handleDisconnect();
 
 
+// code in case the user leave the login space empty
 
-// connection.connect((error) => {
-//     if (error) {
-//         console.log(error);
-//     } else {
-//         console.log('MySQL nodejs-login connected...')
-//     }
-// });
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).render('login', {
+                message: 'Please provide an email and password'
+            })
+        }
+
+        connection.query('SELECT * FROM login WHERE email=?', [email], async (error, results) => {
+            console.log(results);
+            if (!results || !(await bcrypt.compare(password, results[0].password))) {
+                res.status(401).render('login', {
+                    message: 'Email or Password is incorrect'
+                })
+            } else {
+                const id = results[0].id;
+                const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+                    expiresIn: process.env.JWT_EXPIRES_IN
+                });
+
+                console.log('the token is:' + token);
+                const cookieOptions = {
+                    expires: new Date(
+                        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+                    ),
+                    httpOnly: true
+                }
+                res.cookie('jwt', token, cookieOptions);
+                res.status(200).redirect('/ui');
+            }
+
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
 
 
-// // // home page
-// // exports.view = (req, res) => {
-// // res.render('home');
-// // }
+
+connection.connect((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('MySQL nodejs-login connected...')
+    }
+});
 
 
-// exports.register = (req, res) => {
-//     console.log(req.body);
-
-//     // is replaced by Destructor
-//     // const name = req.body.name;
-//     // const email = req.body.email;
-//     // const password = req.body.password;
-//     // const passwordConfirm = req.body.passwordConfirm;
-
-//     // Destructor
-//     const { name, email, password, passwordConfirm } = req.body;
-//     //query that order to MySQL to get the user email only once
-//     connection.query('SELECT email FROM login WHERE email = ?', [email], async (error, results) => {
-//         if (error) {
-//             console.log(error);
-//         }
-//         if (results.length > 0) {
-//             return res.render('register', {
-//                 message: 'That email is already in use'
-//             })
-//         } else if (password !== passwordConfirm) {
-//             return res.render('register', {
-//                 message: 'Password do not match'
-//             });
-//         }
-
-//         let hashedPassword = await bcrypt.hash(password, 8);
-//         console.log(hashedPassword);
-
-//         connection.query('INSERT INTO login SET ?', { name: name, email: email, password: hashedPassword }, (error, results) => {
-//             if (error) {
-//                 console.log(error);
-//             } else {
-//                 console.log(results);
-//                 return res.render('register', {
-//                     message: 'User registered'
-//                 });
-
-//             }
-//         })
-
-//     });
-
+// // home page
+// exports.view = (req, res) => {
+// res.render('home');
 // }
 
 
-// exports.isLoggedIn = async (req, res, next) => {
-//     // console.log(req.cookies);
-//     if (req.cookies.jwt) {
-//         try {
-//             //1)verify the token
-//             const decoded = await promisify(jwt.verify)(req.cookies.jwt,
-//                 process.env.JWT_SECRET
-//             );
+exports.register = (req, res) => {
+    console.log(req.body);
 
-//             console.log(decoded);
-//             //2) Check if the user still exists
-//             connection.query('SELECT * FROM login WHERE id = ?', [decoded.id], (error, result) => {
-//                 console.log(result);
+    // is replaced by Destructor
+    // const name = req.body.name;
+    // const email = req.body.email;
+    // const password = req.body.password;
+    // const passwordConfirm = req.body.passwordConfirm;
 
-//                 if (!result) {
-//                     return next();
-//                 }
-//                 req.user = result[0];
-//                 return next();
-//             });
-//         } catch (error) {
-//             console.log(error);
-//             return next();
-//         }
-//     } else {
-//         next();
-//     }
+    // Destructor
+    const { name, email, password, passwordConfirm } = req.body;
+    //query that order to MySQL to get the user email only once
+    connection.query('SELECT email FROM login WHERE email = ?', [email], async (error, results) => {
+        if (error) {
+            console.log(error);
+        }
+        if (results.length > 0) {
+            return res.render('register', {
+                message: 'That email is already in use'
+            })
+        } else if (password !== passwordConfirm) {
+            return res.render('register', {
+                message: 'Password do not match'
+            });
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
+
+        connection.query('INSERT INTO login SET ?', { name: name, email: email, password: hashedPassword }, (error, results) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(results);
+                return res.render('register', {
+                    message: 'User registered'
+                });
+
+            }
+        })
+
+    });
+
+}
+
+
+exports.isLoggedIn = async (req, res, next) => {
+    // console.log(req.cookies);
+    if (req.cookies.jwt) {
+        try {
+            //1)verify the token
+            const decoded = await promisify(jwt.verify)(req.cookies.jwt,
+                process.env.JWT_SECRET
+            );
+
+            console.log(decoded);
+            //2) Check if the user still exists
+            connection.query('SELECT * FROM login WHERE id = ?', [decoded.id], (error, result) => {
+                console.log(result);
+
+                if (!result) {
+                    return next();
+                }
+                req.user = result[0];
+                return next();
+            });
+        } catch (error) {
+            console.log(error);
+            return next();
+        }
+    } else {
+        next();
+    }
 
 
 
-// }
+}
