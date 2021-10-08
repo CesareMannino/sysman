1622c0bd94b09fa17583e4d56d517bd3569aa4d4
cesare <cesaremannino@hotmail.it>
2021-10-08 09:19:03 +0200

    DB OK login no

--- routes/user.js
+++ routes/user.js
@@ -32,7 +32,7 @@ router.get('/profile', authController.isLoggedIn, (req, res) => {
 });
 
 
-router.get('/ui', authController.isLoggedIn, (req, res,) => {
+router.get('/ui', authController.isLoggedIn,userController.view, (req, res,) => {
   if (req.user) {
     res.render('ui');
   } else {
