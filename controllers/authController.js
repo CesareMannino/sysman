f9d38e8f5b171add64903fc2b6921bcd182d2fc2
cesare <cesaremannino@hotmail.it>
2022-02-25 18:53:46 +0200

    login fully working

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,10 +4,10 @@ const bcrypt = require('bcryptjs');
 const { promisify } = require('util');
 
 var db_config = {
-    host: "us-cdbr-east-04.cleardb.com",
-    user: "bbaaff48f634c6",
-    password: "dacbf7fa",
-    database: "heroku_c7ad469172e97f3"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 };
 
 var connection;
@@ -54,15 +54,16 @@ exports.login = async (req, res) => {
         }
 
         connection.query('SELECT * FROM login WHERE email=?', [email], async (error, results) => {
-            // console.log(results);
+        
             // conditional statement to handle the wrong username error
-            // if (error === null) {
+            console.log("these are the results:",results)
+            // if (results == '') {
             //     res.status(401).render('login', {
             //         message: 'Email or Password is incorrect'
             //     })
             // }
             //conditional if statement to compare password in database and password inserted by the client
-            if (!results || !(await bcrypt.compare(password, results[0].password || error===null))) {
+            if (results == "" || !(await bcrypt.compare(password, results[0].password))) {
                 res.status(401).render('login', {
                     message: 'Email or Password is incorrect'
                 }) //conditional statement to fetch the id of the client and signign in with sign() function
