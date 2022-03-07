2c664994a9f067b234b97415777770dcd6c7113d
Cesare <56283337+CesareMannino@users.noreply.github.com>
2022-03-07 22:03:39 +0200

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -40,7 +40,7 @@ const s3 = new aws.S3({
 });
 
 var uploadFiles = multer({
-  limits: { fileSize: 3000000 }, 
+  limits: { fileSize: 10000000 }, 
   storage:  multerS3({
     s3: s3,
     bucket: 'uploadvms',
